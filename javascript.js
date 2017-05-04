3fc093a191fc51f68cd2b633cd75b5834c337c08
chinchillapsico <coraline.colous@gmail.com>
2017-05-04 10:31:52 -0300

    cambios a js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function pokemon(nombre, color) {
+function pokemon(nombre, color, poderDeAtaque){
 	/*funcion constructora*/
 
 	this.nombre=nombre;
@@ -9,17 +9,29 @@ function pokemon(nombre, color) {
 
 	
 	this.poderDeAtaque= poderDeAtaque;
-	this.mostrarPokemon(){
-		return ("hola soy:"+ this.nombre + "y soy de color"+ this.color)
+	this.mostrarPokemon= function(){
+		return("hola soy:"+ this.nombre + "y soy de color"+ this.color);
 	}
 	this.aumentarAmistad = function(valor){
-		this.nivelDeAmistad = this.nivelDeAmistad+ valor;
+		this.nivelDeAmistad = this.nivelDeAmistad+ valor
     /* este método aumenta el valor nivelDeAmistad (que parte en cero)*/		
 	}
 
 	this.atacar = function(pokemon){
-		pokemon.vida = pokemon.vida - this.poderDeAtaque;
+		pokemon.vida = pokemon.vida - this.poderDeAtaque
+		/* este método le resta a la vida el poder de ataque de su contrincante*/
 	}
 }
 
-var picachu =
+const picachu = new pokemon("picachu", "amarillo", 100);
+const charmander = new pokemon("charmander", "rojo", 20);
+const nidoran = new pokemon("nidoran", "morado", 200);
+const togepi = new pokemon("togepi", "huevito", 10);
+
+
+picachu.atacar(charmander);
+console.log(charmander.vida);
+
+togepi.atacar(nidoran);
+console.log(nidoran.vida);
+
